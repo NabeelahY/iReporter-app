6d5bdd32ea29474e743e1cafd9da88f546963fda
Nabeelah Yousuph <noblenab13@gmail.com>
2018-12-06 20:17:58 +0100

    [#162474880] API endpoints for get all records

---
+++ server/database/db.js
@@ -0,0 +1,33 @@
+module.exports = [
+    {
+        id: 1,
+        createdOn: "January 13th, 2017",
+        createdBy: 023,
+        type: "Red-flag",
+        location: "6.5244 N, 3.3792 E",
+        status: "under investigation",
+        comment: "Police harrased me for not giving them bribe"
+
+    },
+    {
+        id: 2,
+        createdOn: "March 8th, 2018",
+        createdBy: 973,
+        type: "Intervention",
+        location: "6.5244 N, 3.3792 E",
+        status: "resolved",
+        comment: "A bridge collapsed and causing traffic"
+
+    },
+    {
+        id: 3,
+        createdOn: "September 29th, 2018",
+        createdBy: 657,
+        type: "Red-flag",
+        location: "6.5244 N, 3.3792 E",
+        status: "resolved",
+        comment: "I saw JAMB exam officials selling answers to candidates"
+
+    }
+
+];
---
+++ server/routes/api.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const records = require('../database/db');
+const router = express.Router();
+
+
+router.get('/red-flags', function (req, res) {
+	res.status(200).json({
+		status: res.statusCode,
+		data: records
+	});
+});
+
